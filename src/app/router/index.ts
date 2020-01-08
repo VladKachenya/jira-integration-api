37b88327179c2f643b280a9969a6f5060fb4a73e
VladKachenya <38651074+VladKachenya@users.noreply.github.com>
2020-01-08 17:10:40 +0300

    Added trimer and parameters checker for request body

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { getJiraProjectsByCredentials } from "../controllers/ProjectController";
+import { checkCredentialsParams, trimRequestParams } from "../middleware/validators";
 
 
 export default [
@@ -16,6 +17,8 @@ export default [
         path: "/api/v1/projects",
         method: "post",
         handlers: [
+            trimRequestParams,
+            checkCredentialsParams,
             async ({ body }: Request, res: Response) => {
                 const result = await getJiraProjectsByCredentials(body);
                 res.status(200).send(result);
